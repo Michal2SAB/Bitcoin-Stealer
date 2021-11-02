1507301a4eb7def74512d75adc91d9461e9597f0
Michał <38110165+Michal2SAB@users.noreply.github.com>
2021-11-02 14:24:40 +0100

    Update run.sh
    Added comment for people who may not be able to run the script, xterm needs to be installed.

--- Linux/run.sh
+++ Linux/run.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
+# if script don't work, do "apt-get install xterm" in terminal (you need to be root)
 
 echo -e "\e[32mRunning BTC STEALER by Michal2SAB\n"
-xterm -e ./gen.sh # if script don't work, do "apt-get install xterm" in terminal (you need to be root)
+xterm -e ./gen.sh
 nr=0
 while :
 do
     sleep 100
     killall -9 gen.sh
-    xterm -e ./gen.sh # if script don't work, do "apt-get install xterm" in terminal (you need to be root)
+    xterm -e ./gen.sh
     let nr++
     echo "Restarted btc stealer to avoid memory leak issues ($nr)"
 done
