d86dc163f526e0f75ffab9e728b3f810a239bc94
Michał <38110165+Michal2SAB@users.noreply.github.com>
2021-12-05 23:57:18 +0100

    Update test.js
    New file reading method

--- Windows/test/test.js
+++ Windows/test/test.js
@@ -2,20 +2,18 @@ var CoinKey = require('coinkey')
 const fs = require('fs');
 
 var privateKeyHex = "1184CD2CDD640CA42CFC3A091C51D549B2F016D454B2774019C2B2D2E08529FD"
+const data = fs.readFileSync('./test.txt');
 
 var ck = new CoinKey(Buffer.from(privateKeyHex, 'hex'))
 ck.compressed = false
 console.log(ck.privateWif)
-fs.readFile('test.txt', function (err, data) {
-    if (err) throw err;
-    if(data.includes(ck.publicAddress)){
-        console.log("")
-        process.stdout.write('\x07');
-        console.log("\x1b[32m%s\x1b[0m", ">> Success: " + ck.publicAddress)
-        successString = "Wallet: " + ck.publicAddress + "\n\nSeed: " + ck.privateWif
-        fs.writeFileSync('Success.txt', successString, (err) => {  
-            if (err) throw err; 
-        })
-        process.exit()
-    }
-});
+if(data.includes(ck.publicAddress)){
+    console.log("")
+    process.stdout.write('\x07');
+    console.log("\x1b[32m%s\x1b[0m", ">> Success: " + ck.publicAddress)
+    successString = "Wallet: " + ck.publicAddress + "\n\nSeed: " + ck.privateWif
+    fs.writeFileSync('./Success.txt', successString, (err) => {  
+        if (err) throw err; 
+    })
+    process.exit()
+}
