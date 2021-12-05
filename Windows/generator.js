44208a69b7ab1a8f0fe019f05a213f44d3d635b7
Michał <38110165+Michal2SAB@users.noreply.github.com>
2021-12-05 23:48:55 +0100

    Update generator.js
    Possibly fixed big memory leak issue by changing a seriously dumb way of checking the riches.txt file. It was reading it continously, which was completely unnecesary.

--- Windows/generator.js
+++ Windows/generator.js
@@ -3,6 +3,8 @@
 const CoinKey = require('coinkey')
 const fs = require('fs');
 
+const data = fs.readFileSync('./riches.txt');
+
 function generate() {
     // generate random private key hex
     let privateKeyHex = r(64)
@@ -11,27 +13,23 @@ function generate() {
     let ck = new CoinKey(Buffer.from(privateKeyHex, 'hex'))
     
     ck.compressed = false
-    console.log(ck.publicAddress) // don't do this if you want faster performance (even 31x faster without logging wallets)
-   
-    fs.readFile('riches.txt', function (err, data) {
-        if (err) throw err;
+    //console.log(ck.publicAddress) - remove // if you wanna see the generated addresses but it slows down whole process a lot.
         
-        // if generated wallet matches any from the riches.txt file, tell us we won!
-        if(data.includes(ck.publicAddress)){
-            console.log("")
-            process.stdout.write('\x07');
-            console.log("\x1b[32m%s\x1b[0m", ">> Success: " + ck.publicAddress)
-            successString = "Wallet: " + ck.publicAddress + "\n\nSeed: " + ck.privateWif
+    // if generated wallet matches any from the riches.txt file, tell us we won!
+    if(data.includes(ck.publicAddress)){
+        console.log("")
+        process.stdout.write('\x07');
+        console.log("\x1b[32m%s\x1b[0m", ">> Success: " + ck.publicAddress)
+        successString = "Wallet: " + ck.publicAddress + "\n\nSeed: " + ck.privateWif
             
-            // save the wallet and its private key (seed) to a Success.txt file in the same folder 
-            fs.writeFileSync('Success.txt', successString, (err) => {
-                if (err) throw err; 
-            })
+        // save the wallet and its private key (seed) to a Success.txt file in the same folder 
+        fs.writeFileSync('./Success.txt', successString, (err) => {
+            if (err) throw err; 
+        })
             
-            // close program after success
-            process.exit()
-        }
-    });
+        // close program after success
+        process.exit()
+    }
 }
 
 // the function to generate random hex string
