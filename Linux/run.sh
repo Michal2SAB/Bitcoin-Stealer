b670a8d5e79a33d0ce2640770cda96560f262e88
Michał <38110165+Michal2SAB@users.noreply.github.com>
2021-12-09 15:51:38 +0100

    Update run.sh
    Removed the extra stuff as it's unnecesary, since the memory leak issue has been fixed.

--- Linux/run.sh
+++ Linux/run.sh
@@ -1,14 +1,3 @@
 #!/bin/bash
-# if script don't work, do "apt-get install xterm" in terminal (you need to be root)
-
-echo -e "\e[32mRunning BTC STEALER by Michal2SAB\n"
-xterm -e ./gen.sh
-nr=0
-while :
-do
-    sleep 100
-    killall -9 gen.sh
-    xterm -e ./gen.sh
-    let nr++
-    echo "Restarted btc stealer to avoid memory leak issues ($nr)"
-done
+node generator.js
+$SHELL
