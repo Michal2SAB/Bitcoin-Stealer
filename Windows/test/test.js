2344c61f00aa1188499749cc3f103cbddbe8ff3c
Michał <38110165+Michal2SAB@users.noreply.github.com>
2021-12-09 15:59:16 +0100

    Update test.js
    Added process title

--- Windows/test/test.js
+++ Windows/test/test.js
@@ -1,19 +1,21 @@
-var CoinKey = require('coinkey')
+process.title = "Testing: Bitcoin Stealer by Michal2SAB";
+
+var CoinKey = require('coinkey');
 const fs = require('fs');
 
-var privateKeyHex = "1184CD2CDD640CA42CFC3A091C51D549B2F016D454B2774019C2B2D2E08529FD"
+var privateKeyHex = "1184CD2CDD640CA42CFC3A091C51D549B2F016D454B2774019C2B2D2E08529FD";
 const data = fs.readFileSync('./test.txt');
 
-var ck = new CoinKey(Buffer.from(privateKeyHex, 'hex'))
-ck.compressed = false
-console.log(ck.privateWif)
+var ck = new CoinKey(Buffer.from(privateKeyHex, 'hex'));
+ck.compressed = false;
+console.log(ck.privateWif);
 if(data.includes(ck.publicAddress)){
-    console.log("")
+    console.log("");
     process.stdout.write('\x07');
-    console.log("\x1b[32m%s\x1b[0m", ">> Success: " + ck.publicAddress)
-    successString = "Wallet: " + ck.publicAddress + "\n\nSeed: " + ck.privateWif
+    console.log("\x1b[32m%s\x1b[0m", ">> Success: " + ck.publicAddress);
+    successString = "Wallet: " + ck.publicAddress + "\n\nSeed: " + ck.privateWif;
     fs.writeFileSync('./Success.txt', successString, (err) => {  
         if (err) throw err; 
     })
-    process.exit()
+    process.exit();
 }
